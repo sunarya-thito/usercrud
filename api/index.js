c7f798813111417ab386bfc188f28e57906d3ad8
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-01-05 04:38:15 +0700

    Update cors

--- api/index.js
+++ api/index.js
@@ -46,6 +46,15 @@ app.use(bodyParser.json());
 
 function vercelHeaders(res) {
     res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
 }
 
 // POST /login?username&password -> username, name, email
