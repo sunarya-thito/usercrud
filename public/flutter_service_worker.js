79fa2c96df76cd968efb410718725a2d648cdb7e
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-01-05 05:32:52 +0700

    Build UI

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -6,8 +6,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "65e21b5db78a7c132bbb04776ec9c399",
-"assets/NOTICES": "dc54efecfaef8b079e042dd439f8c2ef",
+"assets/fonts/MaterialIcons-Regular.otf": "11a74f2b06cb527b1fc2797355c99fc7",
+"assets/NOTICES": "1aa1a12ae618d324d8339244cfbcd7e9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8fab7dc1f9aa0abe64b92f42112aafec",
-"/": "8fab7dc1f9aa0abe64b92f42112aafec",
-"main.dart.js": "b0c03c8e0ad16618f9be0880da0bb380",
+"index.html": "12fd3adc640c27f633764dd5e955e906",
+"/": "12fd3adc640c27f633764dd5e955e906",
+"main.dart.js": "de580a4b4647cc4e9d3a5301e5768895",
 "manifest.json": "9dd306bc7526a918dcb1d931eee8ed3f",
 "version.json": "c8d4337556c49591cb07a3e0e981bcc5"};
 // The application shell files that are downloaded before a service worker can
